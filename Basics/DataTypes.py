b8e7e1bef79032813e01bd0ec067db65cfe7515f
Suman Shekhar <suman.sumanshekhar698@gmail.com>
2024-01-14 22:57:44 +0530

    first commit

---
+++ Basics/DataTypes.py
@@ -0,0 +1,17 @@
+# String name = "Simran"//Stricly Typed Lanuage
+age = 25 #Dynamic Typing
+print(type(age))
+
+name_of_mine ="Simran"
+value_of_pi = 3.14159
+
+# Python is shudh 100% object oriented
+print(type(value_of_pi))
+
+is_python_easy = True
+print(type(is_python_easy))
+
+complex_number = 7 + 8j
+
+
+
